d74fb94f2408a5dbf6f2aa9c9fcf86487cbac068
Павел Бакшеев <sxeiter@mail.ru>
2023-10-31 15:45:07 +0500

    исправил ошибку

--- game.js
+++ game.js
@@ -1,79 +1,106 @@
+'use strict';
+
 (function() {
-  function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-  
-  function game() {
-    const score = {
-      player: 5,
-      computer: 5,
-    }
+
+    const game = () => {
+
+        const balls = {
+            player: 5,
+            computer: 5,
+            
+            get score () {
+                if (balls.player > balls.computer) {
+                    return 'вы выиграли';
+                } else if (balls.player < balls.computer) {
+                    return 'вы проиграли';
+                } else {
+                    return 'ничья';
+                }
+            },
+        };
+        
+        const botRandom = () => {
+            const randomFoo = Math.floor((Math.random() * 2 + 1));
+            const evenCheck = randomFoo === 2 ? 'even' : 'odd'; 
     
-    return function startGame() {
-      const playerNum = prompt(`Введите число от 1 до ${score.player}`);
-      console.log(`Число игрока: ${playerNum}`);
-      
-      if (playerNum === null) {
-        if (confirm('Хотите завершить игру?')) {
-          if (score.player === score.computer) {
-            alert(`Ничья со счетом: Игрок ${score.player} Компьютер ${score.computer}`);
-            return;
-          }
-          if (score.player > score.computer) {
-            alert(`Вы выиграли со счетом: Игрок ${score.player} Компьютер ${score.computer}`);
-            return;
-          }
-          if (score.player < score.computer) {
-            alert(`Вы проиграли со счетом: Игрок ${score.player} Компьютер ${score.computer}`);
-            return;
-          }
-        }
-      }
-      if (playerNum >= score.player) {
-        alert(`неверное значение, введите число от 1 до ${score.player}!`);
-        return startGame();
-      }
-      
-      const randomNum = getRandomIntInclusive(1, 2);
-      console.log(`Случайное число: ${randomNum}`);
-      
-      const parsedPlayerNum = parseInt(playerNum);
-      
-      if (!Number.isNaN(parsedPlayerNum) && typeof parsedPlayerNum === 'number' && parsedPlayerNum % randomNum === 0) {
-        score.computer += parsedPlayerNum;
-        score.player -= parsedPlayerNum;
-        
-        if (score.player <= 1) {
-          alert('Вы проиграли');
-                    
-          if (confirm('Хотите начать новую игру?')) {
-            score.player = 5;
-            score.computer = 5;
-          } else {
-            return;
-          }
-        }
-      } else if (!Number.isNaN(parsedPlayerNum) && typeof parsedPlayerNum === 'number' && parsedPlayerNum % randomNum !== 0) {
-        score.player += parsedPlayerNum;
-        score.computer -= parsedPlayerNum;
-        
-        if (score.computer === 0) {
-          alert('Вы победили');
-          
-          if (confirm('Хотите начать новую игру?')) {
-            score.player = 5;
-            score.computer = 5;
-          } else {
-            return;
-          }
-        }  
-      } 
-      
-      return startGame();
-    }
-  }
-  
-  window.MARB = game;
+            return evenCheck;
+        };
+
+        const startOver = () => {
+            balls.player = 5;
+            balls.computer = 5;
+        };
+
+    return function start() {
+
+        let userNum = prompt(`Загадайте число шариков от 1 до ${balls.player}`)
+        console.log('userNum: ', userNum);
+
+            if (userNum === null) {
+                const r = confirm('Вы точно хотите выйти?'); 
+                if (r === true) {
+                    alert(`Ваш счет: \r\n Вы: ${balls.player} \r\n Компьютер: ${balls.computer} \r\n Счет: ${balls.score}`)
+                    return null;
+                } else {
+                    return start();
+                };
+            } else if (!(userNum <= balls.player && userNum >= 1)) {
+                alert(`Нужно ввести число от 1 до ${balls.player}`)
+                return start();
+            };
+
+        const botRandomConst = botRandom();
+        console.log('botRandomConst: ', botRandomConst);
+
+        const minNumCheck = () => {
+            if (balls.player < 1) {
+                const cancel = confirm(`Вы проиграли. У вас не осталось шаров.\r\nВаш счет: \r\n Вы: ${balls.player} \r\n Компьютер: ${balls.computer} \r\n Счет: ${balls.score}\r\nХотите сыграть еще разок?`);
+                if (cancel !== true) {
+                    return null;
+                } 
+                else {
+                    startOver();
+                    start();
+                };
+            } else if (balls.computer < 1) {
+                const cancel = confirm(`Вы выиграли. У компьютера не осталось шаров.\r\nВаш счет: \r\n Вы: ${balls.player} \r\n Компьютер: ${balls.computer} \r\n Счет: ${balls.score}\r\nХотите сыграть еще разок?`);
+                if (cancel !== true) {
+                    return null;
+                } 
+                else {
+                    startOver();
+                    start();
+                };
+            } else {
+                start();
+            };
+        };
+
+            if ((!(userNum % 2) && (botRandomConst === 'even')) || ((userNum % 2) && (botRandomConst === 'odd'))) {
+                alert('Бот угадал!');
+                balls.player -= +userNum;
+                console.log('balls.player: ', balls.player);
+                balls.computer += +userNum;
+                console.log('balls.computer: ', balls.computer);
+            } else {
+                alert('Бот проиграл!')
+                    if (userNum < balls.computer) {
+                        balls.player += +userNum;
+                        console.log('balls.player: ', balls.player);
+                        balls.computer -= +userNum;
+                        console.log('balls.computer: ', balls.computer);
+                    } else {
+                        balls.player += balls.computer;
+                        console.log('balls.player: ', balls.player);
+                        balls.computer = 0;
+                        console.log('balls.computer: ', balls.computer);
+                    }
+            };
+
+        minNumCheck();
+    };
+    };
+
+    window.marble = game;
+
 })();
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
 
-const gameStart = window.MARB();
-gameStart();
+const startGame = window.marble();
+startGame();
